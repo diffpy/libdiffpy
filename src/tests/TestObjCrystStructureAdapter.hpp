eacb644b61aec0cdace6c9e8024359fad62d82b9
Pavol Juhas <pavol.juhas@gmail.com>
2010-04-19 17:06:50 +0000

    LiTaO3 unit tests for ObjCryst structure adapter.

--- src/tests/TestObjCrystStructureAdapter.hpp
+++ src/tests/TestObjCrystStructureAdapter.hpp
@@ -289,6 +289,66 @@ class TestObjCrystStructureBondGenerator : public CxxTest::TestSuite
             TS_ASSERT_EQUALS(16, countBonds(*bnds));
         }
 
+
+        void test_LiTaO3()
+        {
+            const string lithium = "Li1+";
+            const string tantalum = "Ta5+";
+            const string oxygen = "O2-";
+            const double epsu = 1e-5;
+            auto_ptr<Crystal> litao3(loadTestCrystal("LiTaO3.cif"));
+            auto_ptr<StructureAdapter> stru(createPQAdapter(*litao3));
+            TS_ASSERT_EQUALS(3, stru->countSites());
+            auto_ptr<BaseBondGenerator> bnds(stru->createBondGenerator());
+            bnds->selectAnchorSite(0);
+            bnds->selectSiteRange(0, 3);
+            // there are 3 oxygen neighbors at 2.065
+            bnds->setRmax(2.1);
+            TS_ASSERT_EQUALS(3, countBonds(*bnds));
+            // Li at site 0 is isotropic, oxygens have equal msd-s towards Li
+            for (bnds->rewind(); !bnds->finished(); bnds->next())
+            {
+                TS_ASSERT_EQUALS(lithium, stru->siteAtomType(bnds->site0()));
+                TS_ASSERT_EQUALS(oxygen, stru->siteAtomType(bnds->site1()));
+                TS_ASSERT_DELTA(0.00265968, bnds->msd0(), epsu);
+                TS_ASSERT_DELTA(0.00710945, bnds->msd1(), epsu);
+            }
+            // there are 3 oxygen neighbors at 2.26
+            bnds->setRmin(2.2);
+            bnds->setRmax(2.3);
+            TS_ASSERT_EQUALS(3, countBonds(*bnds));
+            for (bnds->rewind(); !bnds->finished(); bnds->next())
+            {
+                TS_ASSERT_EQUALS(oxygen, stru->siteAtomType(bnds->site1()));
+                TS_ASSERT_DELTA(0.00265968, bnds->msd0(), epsu);
+                TS_ASSERT_DELTA(0.00824319, bnds->msd1(), epsu);
+            }
+            // finally there are 4 Ta neighbors between 2.8 and 3.1
+            bnds->setRmin(2.8);
+            bnds->setRmax(3.1);
+            TS_ASSERT_EQUALS(4, countBonds(*bnds));
+            for (bnds->rewind(); !bnds->finished(); bnds->next())
+            {
+                TS_ASSERT_DELTA(0.00265968, bnds->msd0(), epsu);
+                TS_ASSERT_EQUALS(tantalum, stru->siteAtomType(bnds->site1()));
+                R3::Vector r01xy = bnds->r01();
+                r01xy[2] = 0.0;
+                // for the tantalum above Li the msd equals U33
+                if (R3::norm(r01xy) < 0.1)
+                {
+                    TS_ASSERT_DELTA(0.00356, bnds->msd1(), epsu);
+                }
+                // other 3 tantalums are related by tripple axis and
+                // have the same msd towards the central Li
+                else
+                {
+                    TS_ASSERT_DELTA(0.00486942, bnds->msd1(), epsu);
+                }
+            }
+        }
+
+
+
 };  // class TestObjCrystStructureBondGenerator
 
 // End of file
