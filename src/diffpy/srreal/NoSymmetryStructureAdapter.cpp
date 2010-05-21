7751efe82bcda4fc5497a2665ffcaeeefa3f0639
Pavol Juhas <pavol.juhas@gmail.com>
2010-05-21 03:45:35 +0000

    StructureAdapter that disables all symmetry operations (space group symmetries and periodic boundary conditions) in crystal structure adapters.

---
+++ src/diffpy/srreal/NoSymmetryStructureAdapter.cpp
@@ -0,0 +1,114 @@
+/*****************************************************************************
+*
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2010 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
+* class NoSymmetryStructureAdapter -- StructureAdapter class that removes
+*     any symmetry expansions (rotations or periodic translations) from
+*     another StructureAdapter instance.  This can be used to use only
+*     the asymmetric unit from any adapter to crystal structure.
+
+* class NoSymmetryBondGenerator -- bond generator
+*
+* nosymmetry -- factory function that creates a NoSymmetryStructureAdapter
+*     instance inside StructureAdapterPtr
+*
+* $Id$
+*
+*****************************************************************************/
+
+#include <diffpy/srreal/NoSymmetryStructureAdapter.hpp>
+
+namespace diffpy {
+namespace srreal {
+
+//////////////////////////////////////////////////////////////////////////////
+// class NoSymmetryStructureAdapter
+//////////////////////////////////////////////////////////////////////////////
+
+// Constructor ---------------------------------------------------------------
+
+NoSymmetryStructureAdapter::NoSymmetryStructureAdapter(
+        StructureAdapterPtr srcstructure)
+{
+    msrcstructure = srcstructure;
+}
+
+// Public Methods ------------------------------------------------------------
+
+BaseBondGenerator* NoSymmetryStructureAdapter::createBondGenerator() const
+{
+    BaseBondGenerator* bnds = new BaseBondGenerator(this);
+    return bnds;
+}
+
+
+int NoSymmetryStructureAdapter::countSites() const
+{
+    return msrcstructure->countSites();
+}
+
+
+double NoSymmetryStructureAdapter::numberDensity() const
+{
+    return 0.0;
+}
+
+
+const R3::Vector& NoSymmetryStructureAdapter::siteCartesianPosition(
+        int idx) const
+{
+    return msrcstructure->siteCartesianPosition(idx);
+}
+
+
+double NoSymmetryStructureAdapter::siteOccupancy(int idx) const
+{
+    return msrcstructure->siteOccupancy(idx);
+}
+
+
+bool NoSymmetryStructureAdapter::siteAnisotropy(int idx) const
+{
+    return msrcstructure->siteAnisotropy(idx);
+}
+
+
+const R3::Matrix& NoSymmetryStructureAdapter::siteCartesianUij(int idx) const
+{
+    return msrcstructure->siteCartesianUij(idx);
+}
+
+
+const std::string& NoSymmetryStructureAdapter::siteAtomType(int idx) const
+{
+    return msrcstructure->siteAtomType(idx);
+}
+
+
+void NoSymmetryStructureAdapter::customPQConfig(PairQuantity* pq) const
+{
+    msrcstructure->customPQConfig(pq);
+}
+
+// Routines ------------------------------------------------------------------
+
+StructureAdapterPtr nosymmetry(StructureAdapterPtr stru)
+{
+    StructureAdapterPtr rv(new NoSymmetryStructureAdapter(stru));
+    return rv;
+}
+
+}   // namespace srreal
+}   // namespace diffpy
+
+// End of file
---
+++ src/diffpy/srreal/NoSymmetryStructureAdapter.hpp
@@ -0,0 +1,79 @@
+/*****************************************************************************
+*
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2010 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
+* class NoSymmetryStructureAdapter -- StructureAdapter class that removes
+*     any symmetry expansions (rotations or periodic translations) from
+*     another StructureAdapter instance.  This can be used to use only
+*     the asymmetric unit from any adapter to crystal structure.
+
+* class NoSymmetryBondGenerator -- bond generator
+*
+* nosymmetry -- factory function that creates a NoSymmetryStructureAdapter
+*     instance inside StructureAdapterPtr
+*
+* $Id$
+*
+*****************************************************************************/
+
+#ifndef NOSYMMETRYSTRUCTUREADAPTER_HPP_INCLUDED
+#define NOSYMMETRYSTRUCTUREADAPTER_HPP_INCLUDED
+
+#include <diffpy/srreal/StructureAdapter.hpp>
+#include <diffpy/srreal/BaseBondGenerator.hpp>
+
+namespace diffpy {
+namespace srreal {
+
+class NoSymmetryStructureAdapter : public StructureAdapter
+{
+    public:
+
+        // constructors
+        NoSymmetryStructureAdapter(StructureAdapterPtr);
+
+        // methods - overloaded
+        virtual BaseBondGenerator* createBondGenerator() const;
+        virtual int countSites() const;
+        virtual double numberDensity() const;
+        virtual const R3::Vector& siteCartesianPosition(int idx) const;
+        virtual double siteOccupancy(int idx) const;
+        virtual bool siteAnisotropy(int idx) const;
+        virtual const R3::Matrix& siteCartesianUij(int idx) const;
+        virtual const std::string& siteAtomType(int idx) const;
+        virtual void customPQConfig(PairQuantity* pq) const;
+
+    private:
+
+        // data
+        StructureAdapterPtr msrcstructure;
+};
+
+// Routines ------------------------------------------------------------------
+
+/// create NoSymmetryStructureAdapter from an existing StructureAdapter
+StructureAdapterPtr nosymmetry(StructureAdapterPtr stru);
+
+
+/// create NoSymmetryStructureAdapter from an adaptable structure object
+template <class T>
+StructureAdapterPtr nosymmetry(const T& stru)
+{
+    ConstStructureAdapterPtr bstru = createStructureAdapter(stru);
+    return nosymmetry(bstru);
+}
+
+}   // namespace srreal
+}   // namespace diffpy
+
+#endif  // NOSYMMETRYSTRUCTUREADAPTER_HPP_INCLUDED
