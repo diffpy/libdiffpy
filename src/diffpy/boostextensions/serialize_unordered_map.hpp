b0adb0b7d2de888322be0d32cac7c29c1aa78d4a
Pavol Juhas <pavol.juhas@gmail.com>
2011-01-05 22:35:53 +0000

    Fixed file comments and formatting, no change in the code.

--- src/diffpy/boostextensions/serialize_unordered_map.hpp
+++ src/diffpy/boostextensions/serialize_unordered_map.hpp
@@ -1,7 +1,19 @@
 /*****************************************************************************
 *
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2010 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
 * Boost serialization support for unordered_map and unordered_multimap.
-* Converted from <boost/serialization/hash_map.hpp>
+* Workaround with conversion to STL list.
 *
 * $Id$
 *
@@ -10,30 +22,15 @@
 #ifndef SERIALIZE_UNORDERED_MAP_HPP_INCLUDED
 #define SERIALIZE_UNORDERED_MAP_HPP_INCLUDED
 
-// MS compatible compilers support #pragma once
-#if defined(_MSC_VER) && (_MSC_VER >= 1020)
-# pragma once
-#endif
-
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
-// serialization/hash_map.hpp:
-// serialization for stl hash_map templates
-
-// (C) Copyright 2002 Robert Ramey - http://www.rrsd.com .
-// Use, modification and distribution is subject to the Boost Software
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-
-//  See http://www.boost.org for updates, documentation, and revision history.
-
 #include <boost/config.hpp>
 #include <boost/unordered_map.hpp>
-#include <boost/serialization/utility.hpp>
 #include <boost/serialization/list.hpp>
 #include <boost/serialization/split_free.hpp>
 
 namespace boost {
 namespace serialization {
+
+// unordered_map -------------------------------------------------------------
 
 template<
     class Archive,
@@ -54,6 +51,7 @@ inline void save(
     std::list<value_type, Alloc> values(t.begin(), t.end());
     ar & values;
 }
+
 
 template<
     class Archive,
@@ -79,6 +77,7 @@ inline void load(
 
 // split non-intrusive serialization function member into separate
 // non intrusive save/load member functions
+
 template<
     class Archive,
     class Key,
@@ -97,7 +96,8 @@ inline void serialize(
     boost::serialization::split_free(ar, t, file_version);
 }
 
-// unordered_multimap
+// unordered_multimap --------------------------------------------------------
+
 template<
     class Archive,
     class Key,
@@ -114,9 +114,10 @@ inline void save(
     const unsigned int file_version
 ){
     typedef std::pair<const Key, T> value_type;
-    std::list<value_type, Alloc> values(t.begin(), t.end()); 
+    std::list<value_type, Alloc> values(t.begin(), t.end());
     ar & values;
 }
+
 
 template<
     class Archive,
@@ -140,9 +141,9 @@ inline void load(
     t.insert(values.begin(), values.end());
 }
 
-
 // split non-intrusive serialization function member into separate
 // non intrusive save/load member functions
+
 template<
     class Archive,
     class Key,
--- src/diffpy/boostextensions/serialize_unordered_set.hpp
+++ src/diffpy/boostextensions/serialize_unordered_set.hpp
@@ -1,7 +1,19 @@
 /*****************************************************************************
 *
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2010 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
 * Boost serialization support for unordered_set and unordered_multiset.
-* Converted from <boost/serialization/hash_set.hpp>
+* Workaround with conversion to STL list.
 *
 * $Id$
 *
@@ -10,28 +22,15 @@
 #ifndef SERIALIZE_UNORDERED_SET_HPP_INCLUDED
 #define SERIALIZE_UNORDERED_SET_HPP_INCLUDED
 
-// MS compatible compilers support #pragma once
-#if defined(_MSC_VER) && (_MSC_VER >= 1020)
-# pragma once
-#endif
-
-/////////1/////////2/////////3/////////4/////////5/////////6/////////7/////////8
-// hash_set.hpp: serialization for stl hash_set templates
-
-// (C) Copyright 2002 Robert Ramey - http://www.rrsd.com . 
-// Use, modification and distribution is subject to the Boost Software
-// License, Version 1.0. (See accompanying file LICENSE_1_0.txt or copy at
-// http://www.boost.org/LICENSE_1_0.txt)
-
-//  See http://www.boost.org for updates, documentation, and revision history.
-
 #include <boost/config.hpp>
 #include <boost/unordered_set.hpp>
 #include <boost/serialization/list.hpp>
 #include <boost/serialization/split_free.hpp>
 
-namespace boost { 
+namespace boost {
 namespace serialization {
+
+// unordered_set -------------------------------------------------------------
 
 template<
     class Archive,
@@ -51,10 +50,11 @@ inline void save(
     ar & values;
 }
 
+
 template<
-    class Archive, 
-    class T, 
-    class Hash, 
+    class Archive,
+    class T,
+    class Hash,
     class Pred,
     class Alloc
 >
@@ -73,10 +73,11 @@ inline void load(
 
 // split non-intrusive serialization function member into separate
 // non intrusive save/load member functions
+
 template<
-    class Archive, 
-    class T, 
-    class Hash, 
+    class Archive,
+    class T,
+    class Hash,
     class Pred,
     class Alloc
 >
@@ -90,7 +91,8 @@ inline void serialize(
     boost::serialization::split_free(ar, t, file_version);
 }
 
-// hash_multiset
+// unordered_multiset --------------------------------------------------------
+
 template<
     class Archive,
     class T,
@@ -105,14 +107,15 @@ inline void save(
     > &t,
     const unsigned int file_version
 ){
-    std::list<T, Alloc> values(t.begin(), t.end()); 
+    std::list<T, Alloc> values(t.begin(), t.end());
     ar & values;
 }
 
+
 template<
-    class Archive, 
-    class T, 
-    class Hash, 
+    class Archive,
+    class T,
+    class Hash,
     class Pred,
     class Alloc
 >
@@ -129,9 +132,9 @@ inline void load(
     t.insert(values.begin(), values.end());
 }
 
-
 // split non-intrusive serialization function member into separate
 // non intrusive save/load member functions
+
 template<
     class Archive,
     class T,
@@ -151,10 +154,5 @@ inline void serialize(
 
 } // namespace serialization
 } // namespace boost
-
-#include <boost/serialization/collection_traits.hpp>
-
-BOOST_SERIALIZATION_COLLECTION_TRAITS(boost::unordered_set)
-BOOST_SERIALIZATION_COLLECTION_TRAITS(boost::unordered_multiset)
 
 #endif  // SERIALIZE_UNORDERED_SET_HPP_INCLUDED
