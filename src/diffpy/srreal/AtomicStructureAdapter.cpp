b6e505f760ffadab598c73e5d4cc634fda574d88
Pavol Juhas <pavol.juhas@gmail.com>
2012-11-01 18:41:06 +0000

    stub for universal structure adapter, non-periodic

---
+++ src/diffpy/srreal/AtomicStructureAdapter.cpp
@@ -0,0 +1,131 @@
+/*****************************************************************************
+*
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2012 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
+* class AtomicStructureAdapter -- universal structure adapter for
+*     a non-periodic set of atoms.
+
+* class AtomicStructureBondGenerator -- bond generator
+*
+* $Id$
+*
+*****************************************************************************/
+
+#include <cassert>
+
+#include <diffpy/serialization.hpp>
+#include <diffpy/srreal/AtomicStructureAdapter.hpp>
+
+using std::string;
+
+namespace diffpy {
+namespace srreal {
+
+//////////////////////////////////////////////////////////////////////////////
+// class AtomicStructureAdapter
+//////////////////////////////////////////////////////////////////////////////
+
+// Public Methods ------------------------------------------------------------
+
+BaseBondGeneratorPtr AtomicStructureAdapter::createBondGenerator() const
+{
+    // FIXME: hack for handling non-periodic structures
+    // should diffpy.Structure get an isPeriodic method?
+    BaseBondGeneratorPtr bnds(
+            new AtomicStructureBondGenerator(shared_from_this()));
+    return bnds;
+}
+
+
+int AtomicStructureAdapter::countSites() const
+{
+    return matoms.size();
+}
+
+
+const R3::Vector& AtomicStructureAdapter::siteCartesianPosition(int idx) const
+{
+    assert(0 <= idx && idx < this->countSites());
+    return matoms[idx].cartesianposition;
+}
+
+
+double AtomicStructureAdapter::siteOccupancy(int idx) const
+{
+    assert(0 <= idx && idx < this->countSites());
+    return matoms[idx].occupancy;
+}
+
+
+bool AtomicStructureAdapter::siteAnisotropy(int idx) const
+{
+    assert(0 <= idx && idx < this->countSites());
+    return matoms[idx].anisotropy;
+}
+
+
+const R3::Matrix& AtomicStructureAdapter::siteCartesianUij(int idx) const
+{
+    assert(0 <= idx && idx < this->countSites());
+    return matoms[idx].cartesianuij;
+}
+
+
+const string& AtomicStructureAdapter::siteAtomType(int idx) const
+{
+    assert(0 <= idx && idx < this->countSites());
+    return matoms[idx].atomtype;
+}
+
+
+void AtomicStructureAdapter::insert(int idx, const AtomAdapter& atom)
+{
+    assert(0 <= idx && idx <= this->countSites());
+    matoms.insert(matoms.begin() + idx, atom);
+}
+
+
+void AtomicStructureAdapter::append(const AtomAdapter& atom)
+{
+    matoms.push_back(atom);
+}
+
+
+void AtomicStructureAdapter::remove(int idx)
+{
+    assert(0 <= idx && idx < this->countSites());
+    matoms.erase(matoms.begin() + idx);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// class AtomicStructureBondGenerator
+//////////////////////////////////////////////////////////////////////////////
+
+// Constructor ---------------------------------------------------------------
+
+AtomicStructureBondGenerator::AtomicStructureBondGenerator(
+        StructureAdapterConstPtr adpt) : BaseBondGenerator(adpt)
+{
+    mastructure = dynamic_cast<const AtomicStructureAdapter*>(adpt.get());
+    assert(mastructure);
+}
+
+}   // namespace srreal
+}   // namespace diffpy
+
+// Serialization -------------------------------------------------------------
+
+DIFFPY_INSTANTIATE_SERIALIZE(diffpy::srreal::AtomicStructureAdapter)
+BOOST_CLASS_EXPORT(diffpy::srreal::AtomicStructureAdapter)
+
+// End of file
---
+++ src/diffpy/srreal/AtomicStructureAdapter.hpp
@@ -0,0 +1,122 @@
+/*****************************************************************************
+*
+* diffpy.srreal     by DANSE Diffraction group
+*                   Simon J. L. Billinge
+*                   (c) 2012 Trustees of the Columbia University
+*                   in the City of New York.  All rights reserved.
+*
+* File coded by:    Pavol Juhas
+*
+* See AUTHORS.txt for a list of people who contributed.
+* See LICENSE.txt for license information.
+*
+******************************************************************************
+*
+* class AtomicStructureAdapter -- universal structure adapter for
+*     a non-periodic set of atoms.
+
+* class AtomicStructureBondGenerator -- bond generator
+*
+* $Id$
+*
+*****************************************************************************/
+
+#ifndef ATOMICSTRUCTUREADAPTER_HPP_INCLUDED
+#define ATOMICSTRUCTUREADAPTER_HPP_INCLUDED
+
+#include <boost/serialization/vector.hpp>
+
+#include <diffpy/srreal/StructureAdapter.hpp>
+
+namespace diffpy {
+namespace srreal {
+
+class AtomAdapter
+{
+    public:
+        
+        // constructor
+        AtomAdapter() :
+            cartesianposition(0.0, 0.0, 0.0),
+            occupancy(1.0),
+            anisotropy(false),
+            cartesianuij(R3::zeros())
+        { };
+
+        // data
+        std::string atomtype;
+        R3::Vector cartesianposition;
+        double occupancy;
+        bool anisotropy;
+        R3::Matrix cartesianuij;
+
+    private:
+
+        // serialization
+        friend class boost::serialization::access;
+        template<class Archive>
+            void serialize(Archive& ar, const unsigned int version)
+        {
+            ar & atomtype;
+            ar & cartesianposition;
+            ar & occupancy;
+            ar & anisotropy;
+            ar & cartesianuij;
+        }
+
+};
+
+
+class AtomicStructureAdapter : public StructureAdapter
+{
+    public:
+
+        // methods - overloaded
+        virtual BaseBondGeneratorPtr createBondGenerator() const;
+        virtual int countSites() const;
+        virtual const R3::Vector& siteCartesianPosition(int idx) const;
+        virtual double siteOccupancy(int idx) const;
+        virtual bool siteAnisotropy(int idx) const;
+        virtual const R3::Matrix& siteCartesianUij(int idx) const;
+        virtual const std::string& siteAtomType(int idx) const;
+
+        // methods - own
+        void insert(int, const AtomAdapter&);
+        void append(const AtomAdapter&);
+        void remove(int);
+
+    private:
+
+        // data
+        std::vector<AtomAdapter> matoms;
+
+        // serialization
+        friend class boost::serialization::access;
+        template<class Archive>
+            void serialize(Archive& ar, const unsigned int version)
+        {
+            ar & boost::serialization::base_object<StructureAdapter>(*this);
+            ar & matoms;
+        }
+
+};
+
+
+class AtomicStructureBondGenerator : public BaseBondGenerator
+{
+    public:
+
+        // constructors
+        AtomicStructureBondGenerator(StructureAdapterConstPtr);
+
+    protected:
+
+        // data
+        const AtomicStructureAdapter* mastructure;
+};
+
+
+}   // namespace srreal
+}   // namespace diffpy
+
+#endif  // ATOMICSTRUCTUREADAPTER_HPP_INCLUDED
