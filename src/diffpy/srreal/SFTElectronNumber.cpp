fe561e7fc8ac183c25dfa9791ad650ecaa317b99
Pavol Juhas <pavol.juhas@gmail.com>
2013-06-26 20:19:21 -0400

    Get ion list from runtime datafile.
    Electron numbers are now obtained from the ionlist.dat data file.

--- src/diffpy/srreal/SFTElectronNumber.cpp
+++ src/diffpy/srreal/SFTElectronNumber.cpp
@@ -23,10 +23,14 @@
 #include <string>
 #include <cstdlib>
 #include <cassert>
+#include <fstream>
 #include <boost/unordered_map.hpp>
+#include <boost/scoped_ptr.hpp>
 
 #include <diffpy/srreal/ScatteringFactorTable.hpp>
 #include <diffpy/srreal/StructureAdapter.hpp>
+#include <diffpy/runtimepath.hpp>
+#include <diffpy/validators.hpp>
 
 using namespace std;
 
@@ -105,137 +109,52 @@ class SFTElectronNumber : public ScatteringFactorTable
 
         const boost::unordered_map<string,int>& getElectronNumberTable() const
         {
-            static boost::unordered_map<string,int> entable;
-            if (entable.empty())
+            using diffpy::runtimepath::datapath;
+            using diffpy::validators::ensureFileOK;
+            typedef boost::unordered_map<string,int> mapping;
+            static boost::scoped_ptr<mapping> entable;
+            if (!entable)
             {
-                const char** line;
-                for (line = mendata_lines; *line != NULL; ++line)
+                entable.reset(new mapping);
+                string ionfile = datapath("ionlist.dat");
+                ifstream fp0(ionfile.c_str());
+                ensureFileOK(ionfile, fp0);
+                string line;
+                int lineno = 0;
+                while (getline(fp0, line))
                 {
-                    istringstream fp(*line);
+                    ++lineno;
+                    istringstream fp(line);
                     string element;
                     int z = 0;
-                    fp >> element >> z;
-                    assert(fp);
-                    entable[element] = z;
+                    fp >> element;
+                    if (element.empty() || element[0] == '#')  continue;
+                    fp >> z;
+                    if (!fp)  this->throwBadLine(ionfile, lineno);
+                    (*entable)[element] = z;
                     for (int v; fp >> v;)
                     {
                         ostringstream smbl;
                         smbl << element << abs(v) << ((v > 0) ? '+' : '-');
-                        entable[smbl.str()] = z - v;
+                        (*entable)[smbl.str()] = z - v;
                     }
                 }
-                assert(436 == entable.size());
+                assert(436 <= entable->size());
             }
-            return entable;
+            return *entable;
         }
 
+
+        void throwBadLine(const string& fname, int lineno) const
+        {
+            ostringstream emsg;
+            emsg << "Invalid data format in " <<
+                fname << " line " << lineno << '.';
+            throw runtime_error(emsg.str());
+        }
+
+
 };  // class SFTElectronNumber
-
-// Ions from bvparm2009cif_lines and Python periodictable package ------------
-
-const char* SFTElectronNumber::mendata_lines[] = {
-    // element atomic-number list-of-ions
-    "H 1 -1 +1",
-    "He 2",
-    "Li 3 -1 +1",
-    "Be 4 -2 +2",
-    "B 5 +3",
-    "C 6 -4 +2 +4",
-    "N 7 -3 -2 +3 +5",
-    "O 8 -2 -1 +1 +2",
-    "F 9 -1 +1",
-    "Ne 10",
-    "Na 11 -1 +1",
-    "Mg 12 -2 +2",
-    "Al 13 -3 +3",
-    "Si 14 -4 +4",
-    "Siva 14",
-    "P 15 -3 +3 +4 +5",
-    "S 16 -4 -2 +2 +4 +6",
-    "Cl 17 -2 -1 +1 +3 +5 +7",
-    "Ar 18",
-    "K 19 -1 +1",
-    "Ca 20 -2 +2",
-    "Sc 21 -3 +3",
-    "Ti 22 -4 -3 -2 +2 +3 +4",
-    "V 23 -5 -3 -2 +1 +2 +3 +4 +5",
-    "Cr 24 -3 -2 +2 +3 +4 +5 +6",
-    "Mn 25 -4 -3 -2 +2 +3 +4 +6 +7",
-    "Fe 26 -3 -2 +2 +3 +4 +6",
-    "Co 27 -3 -2 -1 +1 +2 +3 +4",
-    "Ni 28 -3 -2 +2 +3 +4",
-    "Cu 29 -2 -1 +1 +2 +3",
-    "Zn 30 -2 +2",
-    "Ga 31 -3 +1 +3",
-    "Ge 32 -4 +4",
-    "As 33 -3 +2 +3 +5",
-    "Se 34 -2 -1 +2 +4 +6",
-    "Br 35 -1 +1 +3 +5 +7",
-    "Kr 36 +2",
-    "Rb 37 -1 +1",
-    "Sr 38 -2 +2",
-    "Y 39 +3",
-    "Zr 40 -4 +2 +4",
-    "Nb 41 -5 -3 +3 +4 +5",
-    "Mo 42 -6 -5 -3 +2 +3 +4 +5 +6 +7",
-    "Tc 43 +3 +4 +5 +6 +7",
-    "Ru 44 -4 -3 +2 +3 +4 +5 +6 +7",
-    "Rh 45 -4 -3 +3 +4 +5",
-    "Pd 46 -4 -2 +2 +4",
-    "Ag 47 -2 -1 +1 +2 +3",
-    "Cd 48 -2 +2",
-    "In 49 -3 +1 +3",
-    "Sn 50 -4 -2 +2 +4",
-    "Sb 51 -5 -3 +3 +5",
-    "Te 52 -2 +4 +6",
-    "I 53 -2 -1 +1 +3 +5 +7",
-    "Xe 54 +2 +4 +6 +8",
-    "Cs 55 -1 +1",
-    "Ba 56 -2 +2",
-    "La 57 -3 +3",
-    "Ce 58 -4 -3 +3 +4",
-    "Pr 59 -4 -3 +3",
-    "Nd 60 -3 +2 +3",
-    "Pm 61 -3 +3",
-    "Sm 62 -3 +2 +3",
-    "Eu 63 -3 -2 +2 +3",
-    "Gd 64 -3 +2 +3",
-    "Tb 65 -3 +3",
-    "Dy 66 -3 +2 +3",
-    "Ho 67 -3 +3",
-    "Er 68 -3 +2 +3",
-    "Tm 69 -3 +3",
-    "Yb 70 -3 -2 +2 +3",
-    "Lu 71 -3 +3",
-    "Hf 72 -4 +3 +4",
-    "Ta 73 -5 +4 +5",
-    "W 74 -6 +4 +5 +6",
-    "Re 75 +1 +3 +4 +5 +6 +7",
-    "Os 76 -4 +4 +5 +6 +8",
-    "Ir 77 -4 -3 +4 +5",
-    "Pt 78 -4 -2 +2 +3 +4",
-    "Au 79 -3 -1 +1 +3 +5",
-    "Hg 80 -2 -1 +1 +2",
-    "Tl 81 -3 -1 +1 +3",
-    "Pb 82 -4 -2 +2 +4",
-    "Bi 83 -5 -3 +3 +5",
-    "Po 84 +4",
-    "At 85",
-    "Rn 86",
-    "Fr 87",
-    "Ra 88 -2",
-    "Ac 89 -3 +3",
-    "Th 90 -4 +4",
-    "Pa 91 +4 +5",
-    "U 92 -6 -4 -3 +2 +3 +4 +5 +6",
-    "Np 93 -6 -4 -3 +3 +4 +5 +6 +7",
-    "Pu 94 -6 -4 -3 +3 +4 +5 +6 +7",
-    "Am 95 +3 +4 +5 +6",
-    "Cm 96 +3 +4",
-    "Bk 97 +3 +4",
-    "Cf 98 +3 +4",
-    NULL,
-};
 
 // Registration --------------------------------------------------------------
 
