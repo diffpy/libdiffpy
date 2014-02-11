61f50ae267f45b86b347bb0ccea3bc9b37e647b5
Pavol Juhas <pavol.juhas@gmail.com>
2014-02-11 16:30:40 -0500

    Make gitinfo return empty dictionary with no git.
    Also cache git data so it gets executed only once.

--- site_scons/libdiffpybuildutils.py
+++ site_scons/libdiffpybuildutils.py
@@ -11,16 +11,23 @@ def gitinfo():
     'Extract dictionary of version data from git records.'
     import re
     from subprocess import Popen, PIPE
-    kw = dict(stdout=PIPE, cwd=MYDIR)
+    global _cached_gitinfo
+    if _cached_gitinfo is not None:  return _cached_gitinfo
+    rv = _cached_gitinfo = {}
+    nullfile = open(os.devnull, 'w')
+    kw = dict(stdout=PIPE, stderr=nullfile, cwd=MYDIR)
     proc = Popen(['git', 'describe', '--match=v[[:digit:]]*'], **kw)
     desc = proc.stdout.read()
+    if proc.wait():  return gitinfo()
     proc = Popen(['git', 'log', '-1', '--format=%H %ai'], **kw)
     glog = proc.stdout.read()
-    rv = {}
     rv['version'] = '-'.join(desc.strip().split('-')[:2]).lstrip('v')
     rv['commit'], rv['date'] = glog.strip().split(None, 1)
     mx = re.search(r'(?m)^(\d+)\.(\d+)(?:-(\d+))?', rv['version'])
     rv['major'] = int(mx.group(1))
     rv['minor'] = int(mx.group(2))
     rv['number'] = mx.group(3) and int(mx.group(3)) or 0
-    return rv
+    _cached_gitinfo = rv
+    return gitinfo()
+
+_cached_gitinfo = None
