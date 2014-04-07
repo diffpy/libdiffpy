d415994fc44cdd4c7c5f1b0f96e33fa12181f499
Pavol Juhas <pavol.juhas@gmail.com>
2014-04-07 15:08:25 -0400

    Support USEFULLSUM in PQEvaluatorOptimized.
    USEFULLSUM was assumed false till now.

--- src/diffpy/srreal/PQEvaluator.cpp
+++ src/diffpy/srreal/PQEvaluator.cpp
@@ -31,15 +31,38 @@
 
 using namespace std;
 
-// Local Constants -----------------------------------------------------------
-
-namespace {
-// tolerated load variance for splitting outer loop for parallel evaluation
-const double CPU_LOAD_VARIANCE = 0.1;
-}
-
 namespace diffpy {
 namespace srreal {
+
+// Local Constants and Routines ----------------------------------------------
+
+namespace {
+
+// tolerated load variance for splitting outer loop for parallel evaluation
+const double CPU_LOAD_VARIANCE = 0.1;
+
+std::vector<int>
+complementary_indices(const int sz, const std::vector<int>& indices0)
+{
+    std::vector<int> rv;
+    rv.reserve(sz);
+    std::vector<int>::const_iterator ii0 = indices0.begin();
+    for (int k = 0; k < sz; ++k)
+    {
+        if (ii0 == indices0.end() || k < *ii0)
+        {
+            rv.push_back(k);
+        }
+        else
+        {
+            assert(k == *ii0);
+            ++ii0;
+        }
+    }
+    return rv;
+}
+
+}   // namespace
 
 //////////////////////////////////////////////////////////////////////////////
 // class PQEvaluatorBasic
@@ -147,29 +170,54 @@ void PQEvaluatorOptimized::updateValue(
     {
         return this->updateValueCompletely(pq, stru);
     }
+    if (this->getFlag(FIXEDSITEINDEX) &&
+            sd.diffmethod != StructureDifference::Method::SIDEBYSIDE)
+    {
+        return this->updateValueCompletely(pq, stru);
+    }
     // Remove contributions from the extra sites in the old structure
     assert(sd.stru0 == mlast_structure);
     int cntsites0 = sd.stru0->countSites();
     BaseBondGeneratorPtr bnds0 = sd.stru0->createBondGenerator();
     // loop counter
     long n = mcpuindex;
-    // the loop is adjusted to always perform a full sum and to split the
-    // inner loop in case of parallel calculation.
+    bool usefullsum = this->getFlag(USEFULLSUM);
+    // the loop is adjusted according to usefullsum and split within
+    // the outer loop in case of parallel evaluation.
+    std::vector<int> anchors = sd.pop0;
+    std::vector<int> unchanged;
+    if (usefullsum && !sd.pop0.empty())
+    {
+        unchanged = complementary_indices(cntsites0, sd.pop0);
+        anchors.insert(anchors.end(), unchanged.begin(), unchanged.end());
+    }
     bnds0->selectSiteRange(0, cntsites0);
     std::vector<int>::const_iterator ii0;
-    for (ii0 = sd.pop0.begin(); ii0 != sd.pop0.end(); ++ii0)
+    bool needsreselection = usefullsum;
+    for (ii0 = anchors.begin(); ii0 != anchors.end(); ++ii0)
     {
+        if (n++ % mncpu)    continue;
         const int& i0 = *ii0;
         bnds0->selectAnchorSite(i0);
+        // when using full sum, deselect all but popped sites when
+        // anchor is an unchanged atom
+        if (needsreselection && ii0 >= (anchors.begin() + sd.pop0.size()))
+        {
+            std::vector<int>::const_iterator kk = unchanged.begin();
+            for (; kk != unchanged.end(); ++kk)
+            {
+                bnds0->selectSite(*kk, false);
+            }
+            needsreselection = false;
+        }
         for (bnds0->rewind(); !bnds0->finished(); bnds0->next())
         {
-            if (n++ % mncpu)    continue;
             int i1 = bnds0->site1();
             assert(pq.getPairMask(i0, i1));
-            const int summationscale = (i0 == i1) ? -1 : -2;
+            const int summationscale = (usefullsum || i0 == i1) ? -1 : -2;
             pq.addPairContribution(*bnds0, summationscale);
         }
-        bnds0->selectSite(i0, false);
+        if (!usefullsum)  bnds0->selectSite(i0, false);
     }
     // Add contributions from the new atoms in the updated structure
     // save current value to override the resetValue call from setStructure
@@ -188,21 +236,41 @@ void PQEvaluatorOptimized::updateValue(
     BaseBondGeneratorPtr bnds1 = sd.stru1->createBondGenerator();
     bnds1->selectSiteRange(0, cntsites1);
     std::vector<int>::const_iterator ii1;
-    for (ii1 = sd.add1.begin(); ii1 != sd.add1.end(); ++ii1)
+    anchors = sd.add1;
+    unchanged.clear();
+    if (usefullsum && !sd.add1.empty())
     {
-        bnds1->selectSite(*ii1, false);
+        unchanged = complementary_indices(cntsites1, sd.add1);
+        anchors.insert(anchors.end(), unchanged.begin(), unchanged.end());
     }
-    for (ii1 = sd.add1.begin(); ii1 != sd.add1.end(); ++ii1)
+    else
     {
+        for (ii1 = sd.add1.begin(); ii1 != sd.add1.end(); ++ii1)
+        {
+            bnds1->selectSite(*ii1, false);
+        }
+    }
+    needsreselection = usefullsum;
+    for (ii1 = anchors.begin(); ii1 != anchors.end(); ++ii1)
+    {
+        if (n++ % mncpu)    continue;
         const int& i0 = *ii1;
         bnds1->selectAnchorSite(i0);
-        bnds1->selectSite(i0, true);
+        if (!usefullsum)  bnds1->selectSite(i0, true);
+        if (needsreselection && ii1 >= (anchors.begin() + sd.add1.size()))
+        {
+            std::vector<int>::const_iterator kk = unchanged.begin();
+            for (; kk != unchanged.end(); ++kk)
+            {
+                bnds1->selectSite(*kk, false);
+            }
+            needsreselection = false;
+        }
         for (bnds1->rewind(); !bnds1->finished(); bnds1->next())
         {
-            if (n++ % mncpu)    continue;
             int i1 = bnds1->site1();
             assert(pq.getPairMask(i0, i1));
-            const int summationscale = (i0 == i1) ? +1 : +2;
+            const int summationscale = (usefullsum || i0 == i1) ? +1 : +2;
             pq.addPairContribution(*bnds1, summationscale);
         }
     }
