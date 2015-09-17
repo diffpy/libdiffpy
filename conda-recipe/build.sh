2b2ae1991531f2795371f6427f6d69b3ebc85827
Pavol Juhas <pavol.juhas@gmail.com>
2015-09-17 11:03:34 -0400

    Set LD_LIBRARY_PATH during conda build phase.
    gcc complains when shared libraries cannot be resolved
    during link time.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
 export CPATH="${PREFIX}/include:${CPATH}"
-export LIBRARY_PATH="$PREFIX/lib:$LIBRARY_PATH"
+export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"
+
 if [[ `uname` == Linux ]]; then
     export LDFLAGS="${LDFLAGS} "'-Wl,-rpath,\\$$ORIGIN/.'
+    export LD_LIBRARY_PATH="${PREFIX}/lib"
 fi
-MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
-
-scons -j $MYNCPU lib alltests
-
-# Execute unit tests in a modified environment so that Anaconda
-# shared libraries can be found.
 
 if [[ `uname` == Darwin ]]; then
-    DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib" scons test
-else
-    LD_LIBRARY_PATH="${PREFIX}/lib" scons test
+    export DYLD_FALLBACK_LIBRARY_PATH="${PREFIX}/lib"
 fi
 
+MYNCPU=$(( (CPU_COUNT > 4) ? 4 : CPU_COUNT ))
+
+# Build the library and unit test program.
+scons -j $MYNCPU lib alltests
+
+# Execute unit tests.
+scons test
+
+# Install the library.
 scons install prefix=$PREFIX
 
 grep '^#define DIFFPY_VERSION_STR' < "${PREFIX}/include/diffpy/version.hpp" |
