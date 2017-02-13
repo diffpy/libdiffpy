973908865454629afbe7d4926b20f79475bb9405
Pavol Juhas <pavol.juhas@gmail.com>
2017-02-13 13:57:17 -0500

    Fix conda-build failure due to purged work directory.
    Problem: As of conda-build 2.1 the work directory is removed, but the
    sources are needed to build unit tests.
    Solution: Specify `source_files` that are needed for the tests.

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -2,12 +2,12 @@
 
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
-# Remove unit tests from the build phase.
-scons -C "$SRC_DIR" --clean lib alltests
+# Apply sconscript.local customizations.
+cp ${RECIPE_DIR}/sconscript.local ./
 
 # Build the unit tests program using the installed library.
-scons -C "$SRC_DIR" -j $MYNCPU alltests prefix=$PREFIX test_installed=true
+scons -j $MYNCPU alltests prefix=$PREFIX test_installed=true
 
 # Execute the unit tests.
-MYALLTESTSFAST=$(ls -t ${SRC_DIR}/build/fast*/tests/alltests | head -1)
+MYALLTESTSFAST=$(ls -t ${PWD}/build/fast*/tests/alltests | head -1)
 ${MYALLTESTSFAST}
