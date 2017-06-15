5d009c60fc2862049d196fac7ca07c00c6dbc800
Pavol Juhas <pavol.juhas@gmail.com>
2017-06-15 02:21:29 -0400

    BLD: fix compiler paths for conda package test.
    When `test_installed=true` the scons `prefix` path specifies where
    is the tested diffpy library.  However, it is still necessary to
    set CPATH, LIBRARY_PATH so that boost is found in the configure
    step.

--- conda-recipe/run_test.sh
+++ conda-recipe/run_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export CPATH="${PREFIX}/include:${CPATH}"
+export LIBRARY_PATH="${PREFIX}/lib:${LIBRARY_PATH}"
+
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
 # Apply sconscript.local customizations.
