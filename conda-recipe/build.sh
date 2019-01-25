1c4ebe3cb8603c9629e2dade766f088a0b919269
Pavol Juhas <pavol.juhas@gmail.com>
2019-01-25 11:54:54 -0500

    BLD: fix linker flags for OS X conda build
    Keep linkage to libgslcblas which is indirectly used in libgsl.

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -2,6 +2,9 @@
 
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
+# drop linker flags that spuriously remove linkage with libgslcblas
+LDFLAGS="${LDFLAGS/-Wl,-dead_strip_dylibs/}"
+
 # Apply sconscript.local customizations.
 cp ${RECIPE_DIR}/sconscript.local ./
 
