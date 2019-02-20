56772593192fa0717a55610de4cf94eca0742cbf
Pavol Juhas <pavol.juhas@gmail.com>
2019-02-20 16:40:34 -0500

    TST: cover PeakProfile and its derived classes

--- src/tests/TestPeakProfile.hpp
+++ src/tests/TestPeakProfile.hpp
@@ -22,10 +22,13 @@
 
 #include <diffpy/mathutils.hpp>
 #include <diffpy/srreal/PeakProfile.hpp>
+#include <diffpy/srreal/GaussianProfile.hpp>
+#include <diffpy/srreal/CroppedGaussianProfile.hpp>
 #include "serialization_helpers.hpp"
 
 using namespace std;
 using namespace diffpy::srreal;
+using diffpy::mathutils::eps_eq;
 
 
 class TestPeakProfile : public CxxTest::TestSuite
@@ -34,6 +37,7 @@ class TestPeakProfile : public CxxTest::TestSuite
     private:
 
         PeakProfilePtr mpkgauss;
+        PeakProfilePtr mpkgcrop;
         double meps;
 
     public:
@@ -41,6 +45,7 @@ class TestPeakProfile : public CxxTest::TestSuite
         void setUp()
         {
             mpkgauss = PeakProfile::createByType("gaussian");
+            mpkgcrop = PeakProfile::createByType("croppedgaussian");
             meps = 10 * diffpy::mathutils::DOUBLE_EPS;
         }
 
@@ -53,6 +58,31 @@ class TestPeakProfile : public CxxTest::TestSuite
         }
 
 
+        void test_clone()
+        {
+            TS_ASSERT_EQUALS("gaussian", mpkgauss->clone()->type());
+            TS_ASSERT_EQUALS("croppedgaussian", mpkgcrop->clone()->type());
+        }
+
+
+        void test_attributes()
+        {
+            TS_ASSERT_EQUALS(1, mpkgauss->namesOfDoubleAttributes().size());
+            TS_ASSERT_EQUALS(1, mpkgcrop->namesOfDoubleAttributes().size());
+            mpkgauss->setDoubleAttr("peakprecision", 0.05);
+            const double lo0 = mpkgauss->xboundlo(0.2);
+            GaussianProfile g;
+            TS_ASSERT(!eps_eq(lo0, g.xboundlo(0.2)));
+            g.setPrecision(0.05);
+            TS_ASSERT_EQUALS(lo0, g.xboundlo(0.2));
+            mpkgcrop->setDoubleAttr("peakprecision", 0.05);
+            CroppedGaussianProfile fcrop;
+            TS_ASSERT(!eps_eq(fcrop(0.1, 0.2), (*mpkgcrop)(0.1, 0.2)));
+            fcrop.setPrecision(0.05);
+            TS_ASSERT_EQUALS(fcrop(0.1, 0.2), (*mpkgcrop)(0.1, 0.2));
+        }
+
+
         void test_y()
         {
             double Afwhm1 = 2 * sqrt(M_LN2 / M_PI);
@@ -60,6 +90,11 @@ class TestPeakProfile : public CxxTest::TestSuite
             TS_ASSERT_DELTA(Afwhm1, pkgauss(0, 1), meps);
             TS_ASSERT_DELTA(Afwhm1 / 2, pkgauss(+0.5, 1), meps);
             TS_ASSERT_DELTA(Afwhm1 / 2, pkgauss(-0.5, 1), meps);
+            const PeakProfile& pkgcrop = *mpkgcrop;
+            mpkgauss->setDoubleAttr("peakprecision", 1e-6);
+            mpkgcrop->setDoubleAttr("peakprecision", 1e-6);
+            TS_ASSERT(pkgcrop(0, 1) > pkgauss(0, 1) + meps);
+            TS_ASSERT_EQUALS(0, pkgcrop(1, 0.1));
         }
 
 
@@ -119,6 +154,11 @@ class TestPeakProfile : public CxxTest::TestSuite
             TS_ASSERT_DIFFERS(mpkgauss.get(), pk1.get());
             TS_ASSERT_EQUALS(string("gaussian"), pk1->type());
             TS_ASSERT_EQUALS(0.0123, pk1->getPrecision());
+            mpkgcrop->setPrecision(0.004);
+            PeakProfilePtr pk2 = dumpandload(mpkgcrop);
+            TS_ASSERT_EQUALS("croppedgaussian", pk2->type());
+            TS_ASSERT_EQUALS(0.004, pk2->getPrecision());
+            TS_ASSERT_EQUALS((*mpkgcrop)(0.1, 0.2), (*pk2)(0.1, 0.2));
         }
 
 };  // class TestPeakProfile
